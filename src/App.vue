3fca760bc4130355fb334eeadd290fe38c778674
Dread <34528298+islandbitcoin@users.noreply.github.com>
2024-11-25 15:23:05 -0500

    add init step

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,10 @@
   <router-view />
 </template>
 
-<script setup>
-import { onMounted } from 'vue';
-import { useMintsStore } from './stores/mints';
-
-const mintStore = useMintsStore();
-
-onMounted(async () => {
-  await mintStore.init();
-});
-</script>
-
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
 });
 </script>
