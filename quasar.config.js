b7d86652bc71396733bc64c6282d493017fbd3b6
Vandana <forge@getflash.io>
2026-03-25 01:19:31 -0400

    fix(build): fix workboxMode casing for @quasar/app-vite@2 (GenerateSW not generateSW)

--- quasar.config.js
+++ quasar.config.js
@@ -163,7 +163,7 @@ export default configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxMode: "generateSW", // or 'injectManifest'
+      workboxMode: "GenerateSW", // or 'injectManifest'
       injectPwaMetaTags: true,
       swFilename: "sw.js",
       manifestFilename: "manifest.json",
